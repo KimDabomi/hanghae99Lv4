05eed5d54730cb4548c62ffd7125470e196d9d74
heroq <96118954+heroq@users.noreply.github.com>
2024-01-08 19:22:05 +0900

    refactor: 안쓰는 import 삭제

--- src/main/java/org/sparta/hanghae99lv4/controller/CommentController.java
+++ src/main/java/org/sparta/hanghae99lv4/controller/CommentController.java
@@ -6,7 +6,6 @@ import org.sparta.hanghae99lv4.service.CommentService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
